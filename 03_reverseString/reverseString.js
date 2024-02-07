228a0d8339f2ec3a5010c077da68b47348928659
davidlueke <156998496+davidlueke@users.noreply.github.com>
2024-02-07 13:44:27 +0100

    Add function which prints a given string reversed

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,25 @@
-const reverseString = function() {
+const reverseString = function(text) {
 
+    let reversedText = "";
+
+    if (text == ""){
+
+        return ""
+
+    }else{
+
+        let lastLetter = text.at(-1)
+        reversedText += lastLetter
+
+        let count = -1
+        for (i = 0; i < text.length; i++){
+        let part = text.slice(count -1 , count);
+        reversedText += part;
+        count--;
+        }
+        
+        return reversedText
+    }
 };
 
 // Do not edit below this line
