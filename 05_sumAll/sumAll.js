4dbbcfb26040a6d03be77695ed4159af23127a77
davidlueke <156998496+davidlueke@users.noreply.github.com>
2024-02-08 11:22:07 +0100

    Edit function to sum from num1 to num2

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,21 @@
-const sumAll = function() {
+const sumAll = function(num1, num2) {
 
+    if (Number.isInteger(num1) && Number.isInteger(num2) && Math.sign(num1) === 1 && Math.sign(num2) === 1){
+
+        let result = 0
+        if (num1 > num2){
+            for (i = num2; i <= num1; i++){
+                result += i
+            }
+        } else {
+            for (i = num1; i <=  num2; i++){
+                result += i
+            }
+        }
+        return result
+    }else{
+        return "ERROR"
+    }
 };
 
 // Do not edit below this line
