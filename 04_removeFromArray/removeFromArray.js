a9942d0b855601b58eb1fecba090fad45ac87a41
davidlueke <156998496+davidlueke@users.noreply.github.com>
2024-02-08 10:44:09 +0100

    Edit function to filter an array by given items

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,10 @@
-const removeFromArray = function() {
-
+const removeFromArray = function(array, ...items) {
+    const toRemove = new Set(items);
+    const filteredArray = array.filter( x => !toRemove.has(x));
+    return filteredArray;
+    //accept multiple options to remove
+    //loop through the array to find items to remove
+    //return array without removed items
 };
 
 // Do not edit below this line
