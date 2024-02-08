fd477bf966c4cbe0f5c06ded2d7c03cd3b466aa2
davidlueke <156998496+davidlueke@users.noreply.github.com>
2024-02-08 20:36:05 +0100

    Edit function to search an array of people for the oldest

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,29 @@
-const findTheOldest = function() {
+const findTheOldest = function(people) {
+    let age = [];
+    let thisYear = new Date().getFullYear()
+    people.forEach((person) => {
 
+        if (!("yearOfDeath" in person)){ 
+            age.push(thisYear - person.yearOfBirth) 
+        
+        }else{
+            age.push(person.yearOfDeath - person.yearOfBirth)   
+        }
+    })
+
+    let oldest = Math.max(...age)
+    const foundOldest = people.find ((person) => {
+
+        if (!("yearOfDeath" in person)){
+            return thisYear - person.yearOfBirth == oldest
+            
+        }else{
+            return person.yearOfDeath - person.yearOfBirth == oldest   
+        }
+
+    })
+
+    return foundOldest
 };
 
 // Do not edit below this line
