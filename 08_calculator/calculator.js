ea6e7821bdceafc2de382bcf93b8184ad53d1f92
davidlueke <156998496+davidlueke@users.noreply.github.com>
2024-02-08 12:46:52 +0100

    Edit function to do the math equation they named after

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,24 +1,39 @@
-const add = function() {
-	
+const add = function(num1, num2) {
+	return num1 + num2
 };
 
-const subtract = function() {
-	
+const subtract = function(num1, num2) {
+	return num1 - num2
 };
 
-const sum = function() {
-	
+const sum = function(arr) {
+	let sum = 0
+  arr.forEach((el) => sum += el)
+  return sum
 };
 
-const multiply = function() {
-
+const multiply = function(arr) {
+  let prod = 1
+  arr.forEach((el) => prod *= el)
+  return prod
 };
 
-const power = function() {
-	
+const power = function(num1, num2) {
+  return num1 ** num2	
 };
 
-const factorial = function() {
+const factorial = function(num) {
+  
+  if ( num === 0){
+    return 1
+
+  }else{
+    let result = 1
+    for (i = 1; i <= num; i++){
+      result *= i
+    }
+    return result
+  }
 	
 };
 
