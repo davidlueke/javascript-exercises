744813ab2a0a54bafe2359f826be644ba75971b9
David Lüke <156998496+davidlueke@users.noreply.github.com>
2024-03-06 19:54:10 +0100

    Edit to pass the Jest test

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,26 +1,29 @@
 const add = function(num1, num2) {
-
+  return num1 + num2
 };
 
 const subtract = function(num1, num2) {
-
+  return num1 - num2
 };
 
 const sum = function(arr) {
-	
+	return arr.reduce((total, num) => total + num, 0)
 };
 
 const multiply = function(arr) {
+  return arr.reduce((total, num) => total * num, 1)
  
 };
 
 const power = function(num1, num2) {
-  
+  return num1 ** num2  
 };
 
 const factorial = function(num) {
-  
-	
+  if (num === 0){
+    return 1;
+  };
+  return num * (factorial(num - 1));
 };
 
 // Do not edit below this line
